ca1048126201298de502f6ccb0efad507a15799b
TsitouhRanjafy <radohery09@gmail.com>
2025-08-06 21:01:30 +0300

    WIP: trace the euler path in the graph (bugs remaining)

--- helper.py
+++ helper.py
@@ -49,19 +49,21 @@ def handle_path_insertion(col, row, event):
 
 def  trace_path(node_pos_1, node_pos_2, color_path, sleep_time_s):
     if node_pos_1[0] == node_pos_2[0]:
-        start = node_pos_1[1] if node_pos_1[1] < node_pos_2[1] else node_pos_2[1]
-        end = node_pos_2[1] if start == node_pos_1[1] else node_pos_1[1]
-        for row in range(start + 1, end):
-            f = pos_to_widget_case.get((node_pos_1[0],  row))
+        j = - (node_pos_1[1] - node_pos_2[1]) / abs(node_pos_1[1] - node_pos_2[1])
+        start_j = node_pos_1[1] + j
+        for tmp in range(abs(node_pos_1[1] - node_pos_2[1]) - 1):
+            f = pos_to_widget_case.get((node_pos_1[0],  start_j))
             f.config(bg=color_path)
+            start_j = start_j + j
             root.update()
             time.sleep(sleep_time_s)
     elif node_pos_1[1] == node_pos_2[1]:
-        start = node_pos_1[0] if node_pos_1[0] < node_pos_2[0] else node_pos_2[0]
-        end = node_pos_2[0] if start == node_pos_1[0] else node_pos_1[0]
-        for col in range(start + 1, end):
-            f = pos_to_widget_case.get((col, node_pos_1[1]))
+        i = - (node_pos_1[0] - node_pos_2[0]) / abs(node_pos_1[0] - node_pos_2[0])
+        start_i = node_pos_1[0] + i
+        for tmp in range(abs(node_pos_1[0] - node_pos_2[0]) - 1):
+            f = pos_to_widget_case.get((start_i, node_pos_1[1]))
             f.config(bg=color_path)
+            start_i = start_i + i
             root.update()
             time.sleep(sleep_time_s)
     elif abs(node_pos_1[0] - node_pos_2[0]) == abs(node_pos_1[1] - node_pos_2[1]):
@@ -69,7 +71,6 @@ def  trace_path(node_pos_1, node_pos_2, color_path, sleep_time_s):
         j = - (node_pos_1[1] - node_pos_2[1]) / abs(node_pos_1[1] - node_pos_2[1])
         start_i = node_pos_1[0] + i
         start_j = node_pos_1[1] + j
-
         for k in range(abs(node_pos_1[0] - node_pos_2[0]) - 1):
             f = pos_to_widget_case.get((start_i, start_j))
             f.config(bg=color_path)
@@ -96,7 +97,7 @@ def  trace_path(node_pos_1, node_pos_2, color_path, sleep_time_s):
             time.sleep(sleep_time_s)
 
 def test(node_a, node_b):
-    trace_path(node_to_xy.get(node_a), node_to_xy.get(node_b), "green", 1)
+    trace_path(node_to_xy.get(node_a), node_to_xy.get(node_b), "green", 0.08)
 
 def toggle_insertion(insert_name_char):
     global current_insertion
@@ -104,11 +105,19 @@ def toggle_insertion(insert_name_char):
 
 
 def search_e_path(event):
-    # print(f"{nodes_adj} {nodes}")
     for node in nodes:
         print(f"{node}: {list(nodes_adj[node])}")
         add_adj(node, list(nodes_adj[node]))
-    print(find_euler_path(nodes))
+    path = find_euler_path(nodes)
+    print(f"path euler: {path}")
+    a = path[0]
+    i = 1
+    while i < len(path):
+        trace_path(node_to_xy.get(a), node_to_xy.get(path[i]), "green", 0.08)
+        a = path[i]
+        print(f"i = {i}")
+        i += 1
+    
     
     
 def search_h_path(event):
--- interface.py
+++ interface.py
@@ -27,6 +27,7 @@ _mylib.HamiltonienPathByFleury.restype = POINTER(c_size_t)
 adj = POINTER(KV_ITEM)()
 item = POINTER(c_size_t)()
 euler_chemin = POINTER(c_size_t)()
+hamiltonian_chemin = POINTER(c_size_t)()
 
 # function
 def add_adj(node, list_adj):
@@ -38,8 +39,12 @@ def add_adj(node, list_adj):
 def find_euler_path(list_node):
     global euler_chemin
     euler_chemin = _mylib.EulerPathByFleury(adj, (c_size_t * len(list_node))(*list_node), len(list_node))
-    return euler_chemin[:len(list_node)]
+    return euler_chemin[:len(list_node) + 1]
 
+def find_hamiltonian_path(list_node):
+    global hamiltonian_chemin
+    hamiltonian_chemin = _mylib.HamiltonienPathByFleury(adj, (c_size_t * len(list_node))(*list_node), len(list_node))
+    return hamiltonian_chemin[:len(list_node) + 1]
 
 
 def print_path(path):
@@ -47,11 +52,13 @@ def print_path(path):
         print(f"i = {i}, node: {_mylib.arr_get(path, i)}")
 
         
-add_adj(0, [9,3,5])
-add_adj(9, [0, 5])
-add_adj(3, [0, 5])
-add_adj(5, [9, 0, 3])
+add_adj(0, [4])
+add_adj(1, [2, 3, 4])
+add_adj(2, [1, 3])
+add_adj(3, [1, 2, 4])
+add_adj(4, [0, 1, 3])
 
+print_path(find_euler_path([0, 1, 2, 3, 4]))
 
 # free
 _mylib.free_graph(adj)
